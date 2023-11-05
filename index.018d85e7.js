83e31f60b80f69b9ffdb37db084d259d249fe231
Code Sandbox <hello@codesandbox.io>
2023-11-05 15:56:45 +0000

    Deployment from Codesandbox.

---
+++ index.018d85e7.js
@@ -0,0 +1,10 @@
+console.log("aaaaああああああ"),document.getElementById("app").innerHTML=`
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;//# sourceMappingURL=index.018d85e7.js.map
+
+//# sourceMappingURL=index.018d85e7.js.map
